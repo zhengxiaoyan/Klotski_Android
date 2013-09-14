a3d452709d8af18ff0ed6668f02afdd4eeb9c2e1
zhengxiaoyan <zhang.bing.xy@gmail.com>
2013-09-14 17:23:14 +0900

    Initial commit

---
+++ src/zb/zxy/zyc/klotski/KlotskiActivity.java
@@ -0,0 +1,22 @@
+package zb.zxy.zyc.klotski;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class KlotskiActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_klotski);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.klotski, menu);
+		return true;
+	}
+
+}
